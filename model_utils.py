5711255191ac98ab637b4723c8fbfe3b470666f9
LachinFernando <106962694+LachinFernando@users.noreply.github.com>
2025-01-09 10:14:42 +0530

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -1,4 +1,4 @@
-#from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip
 from ultralytics import YOLO
 import streamlit as st
 from streamlit_webrtc import VideoTransformerBase
