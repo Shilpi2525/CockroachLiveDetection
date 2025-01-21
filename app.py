80223cf7fe48469756705a97e7a58d4fba452466
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-01-21 11:48:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import streamlit as st
 from PIL import Image
 import io
-import av
-import numpy as np
 import os
 import shutil
 import model_utils
 from model_utils import load_yolo_model
 
-
 MODEL_NAME = "cockroach_detection.pt"
 IMAGE_ADDRESS = "https://i.ytimg.com/vi/bEwCA_nrY5Q/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB4y6YJGxw6oVxFm-Uucbrjfqhu2Q"
 RADIO_IMAGE = "Image"
 RADIO_VIDEO = "Video"
-#RADIO_WEBCAM = "Webcam"
 USER_IMAGE_NAME = "user_input.png"
 USER_VIDEO_NAME = "user_video.avi"
 PREDICTION_PATH = "runs/detect/predict"
@@ -23,17 +19,15 @@ PREDICTION_VIDEO_PATH = PREDICTION_NEW_PATH + "/" + USER_VIDEO_NAME
 FINAL_PREDICTION_VIDEO = "output.mp4"
 VIDEO_EXTENSION = ".avi"
 
-
-#load the pytorch weights
+# Load the PyTorch weights
 model = load_yolo_model(MODEL_NAME)
 
-#get predictions
+# Get predictions
 def get_predictions(source_path):
-    model.predict(source_path, save = True)
+    model.predict(source_path, save=True)
     print("Prediction Complete")
 
-    #copy the predictions and save it independently
-    #this is mainly to avoid prediction tree structure
+    # Copy the predictions and save them independently
     if os.path.exists(PREDICTION_PATH):
         for item in os.listdir(PREDICTION_PATH):
             shutil.copy(os.path.join(PREDICTION_PATH, item), os.path.join(PREDICTION_NEW_PATH, item))
@@ -42,65 +36,61 @@ def get_predictions(source_path):
                 print("Video Conversion Complete!!!!!!")
             os.remove(os.path.join(PREDICTION_PATH, item))
 
-    #then remove the predict director
     os.rmdir(PREDICTION_PATH)
     print("Folder removed!")
 
     return True
 
-
-#web application
+# Web application
 st.title("Cockroach Detection")
 
-#set an image
-st.image(IMAGE_ADDRESS , caption = "Cockroach Detection")
+# Set an image
+st.image(IMAGE_ADDRESS, caption="Cockroach Detection")
 
-#detection dashboard
+# Detection dashboard
 st.header("Detection Dashboard 📷")
 
-
-#sidebar
+# Sidebar
 with st.sidebar:
     results = False
     video_results = False
     user_image = None
     user_video = None
-    #set a header
+
+    # Set a header
     st.header("Cockroach Detection")
 
-    #choose a method
+    # Choose a method
     st.subheader("Select a method")
 
-    #set a radio button
-    option = st.radio("Select an input option", [RADIO_IMAGE , RADIO_VIDEO], captions=["Detect signs on Images", "Detect signs on videos"])
+    # Set a radio button
+    option = st.radio("Select an input option", [RADIO_IMAGE, RADIO_VIDEO], captions=["Detect signs on Images", "Detect signs on videos"])
 
-    #if image
+    # If image
     if option == RADIO_IMAGE:
-        user_image = st.file_uploader("Upload an image", accept_multiple_files=False, help = "Upload any image from your local", type = ["png", "jpg", "jpeg"])
+        user_image = st.file_uploader("Upload an image", accept_multiple_files=False, help="Upload any image from your local", type=["png", "jpg", "jpeg"])
 
         if user_image:
-            if st.button("Detect Signs", use_container_width = True, type = 'primary'):
+            if st.button("Detect Signs", use_container_width=True, type='primary'):
                 results = get_predictions(USER_IMAGE_NAME)
 
     if option == RADIO_VIDEO:
-        user_video = st.file_uploader("Upload a video", accept_multiple_files=False, help = "Upload any video from your local", type = ["mp4", "avi", "mpeg"])
+        user_video = st.file_uploader("Upload a video", accept_multiple_files=False, help="Upload any video from your local", type=["mp4", "avi", "mpeg"])
         if user_video:
-            if st.button("Detect Signs", use_container_width = True, type = 'primary'):
+            if st.button("Detect Signs", use_container_width=True, type='primary'):
                 with st.spinner("Processing the Video...."):
                     video_results = get_predictions(USER_VIDEO_NAME)
 
-
-#create two columns
+# Create two columns
 col1, col2 = st.columns(2)
 
 if user_image:
-
     with col1:
         st.subheader("User Input")
-        #set the user image
+        # Set the user image
         st.image(user_image)
 
-        #read and save the image
+        # Read and save the image
         image_bytes = io.BytesIO(user_image.read())
         input_image = Image.open(image_bytes)
         input_image.save(USER_IMAGE_NAME)
@@ -108,15 +98,15 @@ if user_image:
 if results:
     with col2:
         st.subheader("Prediction")
-        #set the user image
+        # Set the user image
         st.image(PREDICTION_IMAGE_PATH)
 
 if user_video:
-    #set the user video
+    # Set the user video
     st.subheader("User Input Video")
     st.video(user_video)
 
-    #read bytes
+    # Read bytes
     video_bytes = io.BytesIO(user_video.read())
 
     with open(USER_VIDEO_NAME, "wb") as video_file:
@@ -127,5 +117,3 @@ if user_video:
 if video_results:
     st.subheader("Predicted Video")
     st.video(os.path.join(PREDICTION_NEW_PATH, FINAL_PREDICTION_VIDEO))
-
-
