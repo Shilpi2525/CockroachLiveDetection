d18d638b7f1793b39a8673ce2174149230e4b121
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-01-21 11:24:01 +0530

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -1,7 +1,6 @@
 from moviepy.editor import VideoFileClip
 from ultralytics import YOLO
 import streamlit as st
-from streamlit_webrtc import VideoTransformerBase
 import numpy as np
 from PIL import Image
 import av
@@ -27,38 +26,3 @@ def load_yolo_model(model_path):
     return YOLO(model_path)
 
 
-class MyVideoTransformer(VideoTransformerBase):
-    def __init__(self, conf, model):
-        self.conf = conf
-        self.model = model
-
-    def recv(self, frame):
-        # image = frame.to_ndarray(format="bgr24")
-        # processed_image = self._display_detected_frames(image)
-        # st.image(processed_image, caption='Detected Video', channels="BGR", use_column_width=True)
-        # return av.VideoFrame.from_ndarray(processed_image, format="bgr24")
-
-        try:
-            image = frame.to_ndarray(format="bgr24")
-            # Add object detection processing here...
-            return av.VideoFrame.from_ndarray(image, format="bgr24")
-        except Exception as e:
-            logger.error(f"Error in video_frame_callback: {e}")
-            return frame
-
-    def _display_detected_frames(self, image):
-        orig_h, orig_w = image.shape[0:2]
-        width = 720  # Set the desired width for processing
-
-        # cv2.resize used in a forked thread may cause memory leaks
-        input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
-
-        if self.model is not None:
-            # Perform object detection using YOLO model
-            res = self.model.predict(input, conf=self.conf)
-
-            # Plot the detected objects on the video frame
-            res_plotted = res[0].plot()
-            return res_plotted
-
-        return input
